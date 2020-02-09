5eefa471e4b6ee34bbb2a3801a201101275a52a7
endritiyay <endrithyseni1997@gmail.com>
2020-02-09 13:04:41 +0100

    Update ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -9,23 +9,23 @@ export default class ContactForm extends React.Component {
                 <label>Don't fill this out if you're human: <input name="bot-field" /></label>
               </p>
               <p className="form-row">
-                <label className="form-label" htmlFor="contact-user-name">Name</label>
+                <label className="form-label" htmlFor="contact-user-name">Emri</label>
                 <input type="text" name="name" id="contact-user-name" className="form-input" placeholder="Enter your name"/>
                 <span className="input-focus" aria-hidden="true" />
               </p>
               <p className="form-row">
-                <label className="form-label" htmlFor="contact-user-email">Email</label>
+                <label className="form-label" htmlFor="contact-user-email">Email-adresa</label>
                 <input type="email" name="email" id="contact-user-email" className="form-input" placeholder="Enter your email address"/>
                 <span className="input-focus" aria-hidden="true" />
               </p>
               <p className="form-row">
-                <label className="form-label" htmlFor="contact-message">Message</label>
+                <label className="form-label" htmlFor="contact-message">Mesazhi</label>
                 <textarea name="message" id="contact-message" className="form-textarea" rows="5" placeholder="Enter your message" />
                 <span className="input-focus" aria-hidden="true" />
               </p>
               <input type="hidden" name="form-name" value="contactForm" />
               <p className="form-row form-submit">
-                <button type="submit" className="button">Send Message</button>
+                <button type="submit" className="button">Dërgo</button>
               </p>
             </form>
         );
